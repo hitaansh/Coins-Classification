0c94af3f6bc9340a80fae7534060e4be293a2b0a
Hitaansh Gaur <46601485+hitaansh@users.noreply.github.com>
2021-04-24 12:30:54 -0400

    Cleaned up code and added comments

--- main.cpp
+++ main.cpp
@@ -16,135 +16,119 @@
 using namespace cv;
 using namespace std;
 
-std::string filename = "";
+string filename = "";
 cv::Mat ORIGINAL,BW,BLUR,EDGE,TEMP;
 cv::Mat labels, cens;
 vector<Vec3f> circles, temp;
 vector<tuple<double, int>> groups;
 vector<double> radii;
-char midist[50];
-int center;
-int distanceslider = 1;
-int threshold1slider = 100;
-int threshold2slider = 50;
-const int distanceslidermax = 100;
-const int threshold1max = 2000;
-const int threshold2max = 1000;
+int distanceSlider = 1;
+int firstThresholdSlider = 100;
+int secondThresholdSlider = 50;
+const int distanceSliderMax = 100;
+const int firstThresholdMax = 2000;
+const int secondThresholdMax = 1000;
 
-
+// reads in the image
 void readPPM() {
-    //std::cout << "Enter ppm file: ";
-    // std::cin >> filename;
     ORIGINAL = cv::imread(filename, 1);
+    
     // cv::imshow("original image: ", ORIGINAL);
-    //cv::waitKey(2000);
-    std::cout << std::endl;
+    // cv::waitKey(2000);
+    
     //cout << "file name entered: " << filename << endl;
 }
 
+
+// converts the image to black and white
 void convertBW() {
     cv::cvtColor(ORIGINAL, BW, cv::COLOR_BGR2GRAY);
+    
     //cv::imshow("black and white: ", BW);
     //cv::waitKey(2000);
 }
 
+
+// blurs the image (needed to remove noise)
 void blur() {
-    //cv::bilateralFilter(BW, BLUR, 25, 110, 110);
     cv::GaussianBlur(BW, BLUR, cv::Size(45,45), 5);
     
-    
-    //
-    //Mat element = cv::getStructuringElement(1, Size(11,11), Point(-1, -1));
-    //cv::morphologyEx(BLUR, BLUR, cv::MORPH_GRADIENT, element);
-    //cv::morphologyEx(BLUR, BLUR, cv::MORPH_OPEN, element);
+    /* Different Blur and Filtering Techniques*/
+    // cv::bilateralFilter(BW, BLUR, 25, 110, 110);
+    // Mat element = cv::getStructuringElement(1, Size(11,11), Point(-1, -1));
+    // cv::morphologyEx(BLUR, BLUR, cv::MORPH_GRADIENT, element);
+    // cv::morphologyEx(BLUR, BLUR, cv::MORPH_OPEN, element);
     // cv::GaussianBlur(BLUR, BLUR, <#Size ksize#>, <#double sigmaX#>)
-    // for(int x =0; x)
     
     //cv::imshow("blured: ", BLUR);
     //cv::waitKey(0);
 }
 
+// canny edge detection (don't need this)
 void canny() {
     cv::Canny(BLUR, EDGE, 45, 45);
-    // cv::Canny(BW, TEMP, 45, 45);
+    
     cv::imshow("canny edge: ", EDGE);
-    //cv::waitKey(0);
+    cv::waitKey(0);
 }
 
+// removes the circles drawn on the image
 void clear() {
     circles.clear();
     ORIGINAL = cv::imread(filename, 1);
     cv::Canny(BLUR, EDGE, 45, 45);
     
 }
+
+// this the method that implements the trackbar
 static void on_trackbar(int, void* ){
-    //cout << "at on trackbar" << endl;
-//    cv::imshow("CircleDrawing", EDGE);
-//    int rand1 = rand() * 100 + 1;
-//    if (rand1 < 75) {
-//        cout << "";
-//    }
-//    else {
-        //cout << "gonna do hough" << endl;
+    
     clear();
-    // 43,165
-    cout << "Threshold1: " << threshold1slider << " Threshold2: " << threshold2slider << endl;
-        HoughCircles(BLUR, circles, HOUGH_GRADIENT, 5, 125, threshold1slider, threshold2slider,50,150);
-        //cout << "finished hough" << endl;
-        for (const auto& a: circles) {
-            cv::circle(ORIGINAL, Point(a[0],a[1]),a[2], Scalar(255,0,0), 10);
-        }
-        cv::imshow("CircleDrawing", ORIGINAL);
-        
-   // }
+    cout << "Threshold1: " << firstThresholdSlider << " Threshold2: " << secondThresholdSlider << endl;
+    HoughCircles(BLUR, circles, HOUGH_GRADIENT, 5, 125, firstThresholdSlider, secondThresholdSlider,50,150);
+    for (const auto& a: circles) {
+        cv::circle(ORIGINAL, Point(a[0],a[1]),a[2], Scalar(255,0,0), 10);
+    }
+    cv::imshow("CircleDrawing", ORIGINAL);
+
 }
 
-void houghcirclesfinder() {
+// initializes the trackbars and calls the method
+void houghCirclesFinder() {
     cv::namedWindow("CircleDrawing");
-    //cv::createTrackbar("Minimum Distance", "CircleDrawing", &distanceslider, distanceslidermax, on_trackbar);
-    cv::createTrackbar("Threshold1", "CircleDrawing", &threshold1slider, threshold1max, on_trackbar);
-    cv::createTrackbar("Threshold2", "CircleDrawing", &threshold2slider, threshold2max, on_trackbar);
+    //cv::createTrackbar("Minimum Distance", "CircleDrawing", &distanceSlider, distanceSliderMax, on_trackbar);
+    cv::createTrackbar("Threshold1", "CircleDrawing", &firstThresholdSlider, firstThresholdMax, on_trackbar);
+    cv::createTrackbar("Threshold2", "CircleDrawing", &secondThresholdSlider, secondThresholdMax, on_trackbar);
     on_trackbar(0, 0);
     waitKey(0);
-    //HoughCircles(BW, circles, HOUGH_GRADIENT, 5, BW.rows/10, 45, 45, 10, 100);
-    //cout << "created trackbars: " << endl;
-    //cv::imshow("CircleDrawing", EDGE);
-    //cout << "hello after" << endl;
-    //print(circles);
 }
 
-void houghcircles() {
+// runs houghcircles with the appropriate parameters
+void houghCircles() {
     clear();
     HoughCircles(BLUR, circles, HOUGH_GRADIENT, 5, 150, 43, 206, 50, 130);
     // 43, 206
     // 49, 130
 }
 
-
-void drawcircles() {
-    //if(circles.size() <= 70) {
-      //  cout << "circle size: " << circles.size() << endl;
+// draws circles detected by hough circles
+void drawCircles() {
     for (const auto& a: circles) {
         cv::circle(ORIGINAL, Point(a[0],a[1]),a[2], Scalar(255,0,0), 10);
     }
-    //cout << "drawn circles" << endl;
-    // cv::imshow("Circles drawn", ORIGINAL);
-    cv::waitKey(10000);
- //   }
-//    else {
-//        cout << "too many circles " << circles.size() << endl;
-//    }
+    cv::waitKey(0);
 }
 
+// prints the center coordinates for the circles
 void printVector3() {
-    //cout << "hello" << endl;
     cout << circles.size() << endl;
     for (const auto& a: circles) {
         cout << "X center coord: "<< a[0] << " Y center coord: " << a[1] << " Radius: " << a[2] << endl;
     }
 }
 
-void kmeanscluster() {
+// runs kmeans on the circle radii to seperate them into groups
+void kMeansCluster() {
     Mat points(circles.size(), 1, CV_32F);
     for (int x = 0; x < circles.size(); x++) {
         points.at<float>(x) = circles[x][2];
@@ -160,12 +144,14 @@ void kmeanscluster() {
     }
 }
 
-void drawgroups() {
+// draws the coin groups on the image
+void drawGroups() {
     // penny is blue (255, 0, 0)
     // nickle is green (0, 255, 0)
     // quarter is red (0, 0, 255)
-    Mat output;
-    cv::cvtColor(BW, output, cv::COLOR_GRAY2BGR);
+    
+    Mat output = ORIGINAL;
+    // cv::cvtColor(BW, output, cv::COLOR_GRAY2BGR);
     for(int x = 0; x < circles.size(); x++) {
         int current_label = labels.at<int>(x);
         if(current_label == 0) {
@@ -180,16 +166,18 @@ void drawgroups() {
     }
     
     cv::imwrite("./bw.jpg", output);
-    //imshow("amount" , output);
-    //waitKey(0);
+    imshow("amount" , output);
+    waitKey(0);
     
 }
 
-void dollaramount() {
+// calculates the dollar amount present in the picture
+void dollarAmount() {
     double penny = 0.01;
     double nickle = 0.05;
     double quarter = 0.25;
     double finalamount = 0.00;
+    
     for(int x = 0; x < circles.size(); x++) {
         int currgroup = labels.at<int>(x);
         if(currgroup == 0)
@@ -204,21 +192,40 @@ void dollaramount() {
 
 int main(int argc, char **argv)
 {
-    //cout << "at main" << endl;
     filename = argv[1];
-    //cout << filename << endl;
+    
+    // read in the image
     readPPM();
+    
+    // convert it to black and white
     convertBW();
+    
+    // blur the image
     blur();
+    
+    // run canny edge detection on the image
     // canny();
-    //houghcirclesfinder();
-    houghcircles();
+    
+    // help find paramteters for hough circles
+    // houghcirclesfinder();
+    
+    // run hough circles
+    houghCircles();
+    
+    // print coins radii
     // printVector3();
-    //drawcircles();
-    kmeanscluster();
-    dollaramount();
-    drawgroups();
+    
+    // draw detected circles on image
+    // drawcircles();
+    
+    // cluster coins according to radii
+    kMeansCluster();
+    
+    // calcualte dollar amount present in picture
+    dollarAmount();
+    
+    // draw the coins classification on the image
+    drawGroups();
 
     return 0;
-    
 }
